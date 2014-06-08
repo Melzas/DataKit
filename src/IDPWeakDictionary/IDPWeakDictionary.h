d802e9ddb6c95030a59d6038e0abbc4659c9252a
Anton Rayev <sfdsrc@gmail.com>
2014-06-08 21:52:52 +0300

    Add IDPWeakMutableDictionary class

---
+++ src/IDPWeakDictionary/IDPWeakDictionary.h
@@ -0,0 +1,15 @@
+//
+//  IDPWeakMutableDictionary.h
+//  Tixxit
+//
+//  Created by Anton on 08.06.14.
+//  Copyright (c) 2014 Anton Rayev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// dictionary with weak keys and values
+
+@interface IDPWeakMutableDictionary : IDPMutableDictionary
+
+@end
---
+++ src/IDPWeakDictionary/IDPWeakDictionary.m
@@ -0,0 +1,46 @@
+//
+//  IDPWeakMutableDictionary.m
+//  Tixxit
+//
+//  Created by Anton on 08.06.14.
+//  Copyright (c) 2014 Anton Rayev. All rights reserved.
+//
+
+#import "IDPWeakDictionary.h"
+
+#import "IDPWeakArray.h"
+
+@implementation IDPWeakMutableDictionary
+
+#pragma mark -
+#pragma mark NSDictionary
+
+- (id)initWithObjects:(NSArray *)objects forKeys:(NSArray *)keys {
+    self = [super init];
+    if (self) {
+		NSMutableArray *weakObjects = [IDPMutableWeakArray weakArray];
+		[weakObjects addObjectsFromArray:objects];
+		
+        self.dictionary = [NSMutableDictionary dictionaryWithObjects:weakObjects
+                                                             forKeys:keys];
+    }
+    
+    return self;
+}
+
+- (id)objectForKey:(id)aKey {
+    IDPWeakReference *weakReference = [super objectForKey:aKey];
+	
+	return weakReference.object;
+}
+
+#pragma mark -
+#pragma mark NSMutableDictionary
+
+- (void)setObject:(id)anObject forKey:(id <NSCopying>)aKey {
+	IDPWeakReference *weakReference = [IDPWeakReference referenceWithObject:anObject];
+	
+    [super setObject:weakReference forKey:aKey];
+}
+
+@end
